6f93f35ec9fc98c199496fa0c3ecf1bf353e5140
Thomas Harr <xDevThomas@gmail.com>
2023-05-18 17:01:08 +0200

    feat: Redirect medium.com to scribe.rip

---
+++ index.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Medium Redirect to Scribe
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Redirect Medium Article to scribe.rip alternative frontend
+// @author       Thomas Harr
+// @license      MIT
+// @match        https://medium.com/*
+// @run-at       document-start
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    var redirectUrl = location.href.replace(location.host, 'scribe.rip');
+    return location.replace(redirectUrl);
+})();

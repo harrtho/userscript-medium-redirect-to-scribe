98c9b0580be63ce84270ad955a9cba9e15822f71
Thomas Harr <xDevThomas@gmail.com>
2023-05-18 17:14:50 +0200

    feat: Match subdomains of medium.com

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Medium Redirect to Scribe
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.1.1
 // @description  Redirect Medium Article to scribe.rip alternative frontend
 // @author       Thomas Harr
 // @license      MIT
-// @match        https://medium.com/*
+// @match        https://*.medium.com/*
 // @run-at       document-start
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
